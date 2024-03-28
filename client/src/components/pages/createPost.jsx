5c1cfec614c4117603c7056604876d2dbfc0a248
Vrishank Sharma <sharma08462@gmail.com>
2024-03-28 23:45:24 +0530

    Post model

--- client/src/components/pages/createPost.jsx
+++ client/src/components/pages/createPost.jsx
@@ -23,13 +23,30 @@ export default function CreatePost(){
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
-    
+    const [files, setFiles] = useState('');
+
+    async function createNewPost(ev){
+        const data = new FormData;
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+
+        ev.preventDefault();
+        const response = await fetch('http://localhost:8080/post',{
+            method:'POST',
+            body: data,
+        })
+
+        await response.json()
+    }
+
     return(
-        <form>
-            <input type="title" placeholder={'Title'} />
-            <input type="summary" placeholder={'Summary'} />
-            <input type="file"/>
-            <ReactQuill value={content} modules={modules} formats={formats}/>
+        <form onSubmit={createNewPost}>
+            <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)}/>
+            <input type="summary" placeholder={'Summary'} value={summary} onChange={ev => setSummary(ev.target.value)}/>
+            <input type="file" onChange={ ev => setFiles(ev.target.files) }/>
+            <ReactQuill value={content} onChange={newValue => setContent(newValue)} modules={modules} formats={formats}/>
             <button style={{marginTop:'5px'}}>Create Post</button>
         </form>
     )
